6265639b39e4f6486257a2c523decb5406cfd90a
vivids12 <viviane.dsantos@bradesco.com.br>
2026-04-11 17:10:36 -0300

    Implementação do consumidor e produtor de emails com RabbitMQ

--- src/main/java/hub/orcana/orcana_email_service/gateway/ConsumerGateway.java
+++ src/main/java/hub/orcana/orcana_email_service/gateway/ConsumerGateway.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
